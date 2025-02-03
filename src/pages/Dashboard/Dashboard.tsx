e88760a62f789e53d851da9884b3c0a13a0840f3
Bhavana Jami <bhavanajami111@gmail.com>
2025-02-04 01:52:30 +0530

    .

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -12,7 +12,7 @@ import { IndianRupee } from 'lucide-react';
 const Dashboard: React.FC = () => {
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 ">
         <CardDataStats
           title="Total Networth"
           total="$3.456K"
